8a1dec77605a42a29ef5f9fb4cca4c0e02d28ab5
k-tasaka <k.tasaka@genbasupport.com>
2026-08-10 17:38:53 +0900

    石が返せるかどうかの判定を修正 -> 隣が空白でもfalseを返す

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -41,7 +41,6 @@ module ReversiMethods
   end
 
   def put_stone(board, cell_ref, stone_color, dry_run: false)
-    binding.irb
     pos = Position.new(cell_ref)
     raise '無効なポジションです' if pos.invalid?
     raise 'すでに石が置かれています' unless pos.stone_color(board) == BLANK_CELL
@@ -64,6 +63,7 @@ module ReversiMethods
   def turn(board, target_pos, attack_stone_color, direction)
     return false if target_pos.out_of_board?
     return false if target_pos.stone_color(board) == attack_stone_color
+    return false if target_pos.stone_color(board) == BLANK_CELL
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
@@ -79,7 +79,6 @@ module ReversiMethods
   end
 
   def placeable?(board, attack_stone_color)
-    # binding.irb
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         next unless cell == BLANK_CELL
