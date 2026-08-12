2186c185c6a20de6404ae1595786f9af1673c196
k-tasaka <k.tasaka@genbasupport.com>
2026-08-12 10:40:44 +0900

    `placeable?`メソッドの改修（返り値が配列になっていたのを真偽値に修正）

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -76,17 +76,20 @@ module ReversiMethods
 
   def finished?(board)
     !placeable?(board, WHITE_STONE) && !placeable?(board, BLACK_STONE)
+    # binding.break
   end
 
   def placeable?(board, attack_stone_color)
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
+
         next unless cell == BLANK_CELL
 
         position = Position.new(row, col)
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
+    false
   end
 
   def count_stone(board, stone_color)
